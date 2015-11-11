db57f9f1e71eb7cb5c0cfef9f93fc7d1feec1e0d
Thomas Francis Shannon <Thomas@Thomas-MacBook-Pro.local>
2015-11-10 20:39:21 -0800

    added Movies

--- js/main.js
+++ js/main.js
@@ -1,72 +1,66 @@
 $.ajaxSetup({
   headers: {
-    'X-Parse-Application-Id': 'fPIAO5E3YKUXkP8gP9yOK07f65KOiZlrpkdI8jK6',
-    'X-Parse-REST-API-Key': 'LtIQAEjxEXvVkhYyKI6Xb4w1KloLhnVUS3uhzJys'
+    'X-Parse-Application-Id': '24nAZjkW4b1FG2A8Syz2nsLoFI9C6YylSeFq7gy4',
+    'X-Parse-REST-API-Key': 'QizoxovCQQl1DdiLXyfJ64MMoFC0lnjqNT9OEXvJ'
   }
 });
 
 
-var Song = Backbone.Model.extend({
+var Movie = Backbone.Model.extend({
   initialize: function () {
-    console.log("A new song has been created");
+    console.log("A new movie has been created");
   },
-  defaults: {
-    title: null,
-    artist: null,
-    isCool: true,
-    publishYear: null
-  },
-  validate: function (attrs) {
-    if (!attrs.title) {
-      return "Title is required";
-    }
-    if (!attrs.artist) {
-      return "Artist is required";
-    }
-    if (typeof attrs.title !== 'string') {
-      return "Title must a string";
-    }
-    if (typeof attrs.artist !== 'string') {
-      return "Artist must a string";
-    }
-    if (!attrs.publishYear) {
-      return "Published Year is required";
-    }
-    if (isNaN(Number(attrs.publishYear))) {
-      return "Published Year should be a number";
-    }
-  },
-  _parse_class_name: "Song"
+  _parse_class_name:"Movies"
+ 
+ 
+});
+var movie = new Movie({
+  title:"test",
+  genre:"test"
+});
+var movie = new Movie({
+  title:"Se7en",
+  genre:"thriller"
+});
+var movie = new Movie({
+  title:"The Hills Have Eyes",
+  genre:"Horror",
+  director:"I don't know"
+});
+var movie = new Movie({
+  title:"Insidious",
+  genre:"Horror",
+  director:"James Wan"
 });
 
-var song = new Song({
-  title: "Für Elise",
-  artist: "Beethoven",
-  publishYear: 1810
-})
 
-var Songs = Backbone.Collection.extend({
-  model: Song,
-  _parse_class_name: "Song"
+
+var Movies=Backbone.Collection.extend({
+url:"https://test1117.parseapp.com/classes/Movies",
+model: Movie,
+_parse_class_name:"Movies"
+
 });
 
-var SongsCollection = new Songs();
+// Movies.fetch({
+ 
+//       success:function(resp) {
+//         console.log(resp);
+//       }, error:function(err) {
+//         console.log(err)
+//       }
+//     })
+// })
 
-song.save(null, {
+
+movie.save(null, {
   success: function(resp) {
-    console.log(resp)
-
-    SongsCollection.fetch({
-      success: function(resp) {
-        console.log("success: ", resp);
-      }, error: function (err) {
-        console.log("error: ", err);
-      }
-    })
+    console.log("success: ",resp)
   },
-  error: function (err) {
-    console.log(err)
+  error: function(err) {
+    console.log("error: ",err)
   }
 });
+
 
 
